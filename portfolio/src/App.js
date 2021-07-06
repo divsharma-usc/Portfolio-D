88379689bca4135b867975480f257186148f019c
Divyansh <divyansh9595@gmail.com>
2021-07-06 06:30:25 +0530

    minor changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="WelcomeSection">
+        <div className="Introduction">
+          <div className="ProfilePicture">
+            <div>Profile Pic</div>
+            <div>Contacts</div>
+          </div>
+          <div className="Biography">
+            <div>
+              Divyansh
+            </div>
+          </div>
+        </div>
+        <img src="hero (1).svg"></img>
+      </div>
     </div>
   );
 }
