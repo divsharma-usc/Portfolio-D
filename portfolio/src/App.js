dafd745c62b15a12a883692600706c2f7c453ece
Divyansh <divyansh9595@gmail.com>
2022-03-06 19:44:27 +0530

    Removing old changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,25 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Header">
-        <li>Home</li>
-        <li>Education</li>
-        <li>Projects</li>
-        <li>Skills</li>
-      </div>
-      <div className="WelcomeSection">
-        <div className="Introduction">
-          <div className="ProfilePicture">
-            <div>Profile Pic</div>
-            <div>Contacts</div>
-          </div>
-          <div className="Biography">
-            <div>
-              Divyansh is a graduate in Computer Science and Engineering xs
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
