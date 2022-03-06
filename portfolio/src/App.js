c3508c330de7304f572cb1e2d3da75eaf29d2728
Divyansh <divyansh9595@gmail.com>
2022-03-06 21:09:42 +0530

    Making Header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <div id="leftHeaderBox">
+          <a>@div</a>
+        </div>
+
+        <div id="rightHeaderBox">
+          <a>About</a>
+          <a>Education</a>
+          <a>Skills</a>
+          <a>Achievements</a>
+          <a>Contact</a>
+        </div>
+      </div>
+
+      <div className="mainSection">
+      </div>
     </div>
   );
 }
