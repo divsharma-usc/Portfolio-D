55e4824a1fe11229ce611666e5b86e0aa6b1aa73
Divyansh <divyansh9595@gmail.com>
2022-03-06 23:18:38 +0530

    changes to main section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import profileImage from './divImage.png';
+import emailImage from './email.png';
+import linkdIn from './linkdin.png';
+import github from './github.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
@@ -18,6 +23,20 @@ function App() {
       </div>
 
       <div className="mainSection">
+        <div className="midSection">
+          <div id="profileImage">
+            <img src={profileImage} />
+          </div>
+          <div id="mainIntro">
+            <h3>Hi, I'm Divyansh,</h3>
+            <h4>a software developer specalized in back-end development, blockchain technologies and digital assets.</h4>
+            <div>
+              <a><img src={emailImage}/></a>
+              <a><img src={linkdIn}/></a>
+              <a><img src={github}/></a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
